3f52294b43b786f6a6eb36ad0360b2aab6dd2b5d
Artur Ashyrov <artur.ashyrov@mac-studio-artur.home>
2024-10-27 17:03:02 +0100

    Changing the test

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,8 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
+    expect(hello.hello()).toEqual(
+      "Hello World from Czech Republic in the terminal!",
+    );
   });
 });
